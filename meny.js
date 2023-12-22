d214b343eb138dea48ae8bf160b9ca72d20b4ea2
mohammedyasin4 <leonahmad4@gmail.com>
2023-12-22 14:21:49 +0100

    Lagt till en meny för BBQs med Javascript

--- meny.js
+++ meny.js
@@ -1,3 +1,7 @@
+const fileNameWithExtension = window.location.pathname.split('/').pop();
+const fileName = fileNameWithExtension.split('.').slice(0, -1).join('.'); // To use for specific menus
+
+
 function loadMenu() {
     for(const [key, value] of Object.entries(db.pagination)){
         $(".row").append(
@@ -9,3 +13,20 @@ function loadMenu() {
             "</h5></div></div></div>");
     }
 }
+
+function loadSpecificMenu(){
+    for(const [key, value] of Object.entries(db.bbqs)){
+        $(".row").append(
+            "<div class=\"col-6\">" +
+            "<a class=\"clickable-card\" href=\"#\">" +
+            "<div class=\"card p-2 m-2 card-with-bg\" style=\"background-image:url('" + value.img + "');\">"+
+            "<div class=\"card-body\"><h5 class=\"card-title\">" + 
+            value.name + "</h5>" + 
+            "<p>" + value.dsc + "</p>" +
+            "<p>" + value.price + " kr</p>" +
+            "<p>Rating: " + value.rate + "/5</p>" +
+            "<p>Origin: " + value.country + "</p></div>" +
+            "</div></a></div>");
+    }
+}
+
