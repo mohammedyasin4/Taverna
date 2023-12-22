4bb047b421bdd09d6dca928abb2df1d44d2c7347
mohammedyasin4 <leonahmad4@gmail.com>
2023-12-22 11:20:52 +0100

    Menyerna är klickabara och leder till resp. sidor

--- meny.js
+++ meny.js
@@ -2,6 +2,7 @@ function loadMenu() {
     for(const [key, value] of Object.entries(db.pagination)){
         $(".row").append(
             "<div class=\"col-6\">" +
+            "<a href=\"" + key + ".html\">" +
             "<div class=\"card p-2 m-2\">"+
             "<div class=\"card-body\"><h5 class=\"card-title\">" + 
             key.replace(/-/g, ' ').replace(/\b\w/g, (match)=> match.toUpperCase()) + 
